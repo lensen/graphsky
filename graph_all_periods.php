1bec7b8bbf91262f1481dfc7b3cc33be72c8d807
Jeffrey Lensen <jeffrey@hyves.nl>
2012-10-25 10:17:56 +0200

    Oops

--- graph_all_periods.php
+++ graph_all_periods.php
@@ -17,7 +17,7 @@ if ( $cluster == "\*" )
 
 include_once "./header.php";
 
-foreach ($conf["graph_all_periods_timeframes"] as $tf) {i
+foreach ($conf["graph_all_periods_timeframes"] as $tf) {
     $graph_args = get_graph_domainname() . "/graph.php?${graph}${metric}&env=${env}&h=${host}&c=${cluster}&st=${tf}+ago";
     print "<a href=\"${graph_args}&z=xlarge\"><img src=\"${graph_args}&z=large\" /></a>";
 }
