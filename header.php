88f21f28ec5041ffaf27faaf855970c22caad376
Jeffrey Lensen <jeffrey@hyves.nl>
2013-07-12 17:20:47 +0200

    Should fix errors about missing variables, mainly in graph_all_periods. Tweaked the menu_cells abit.

--- header.php
+++ header.php
@@ -1,5 +1,10 @@
 <?php
 $name = "Graphsky";
+$env  = (isset($_GET['env']) && $_GET['env'] != "") ? $_GET['env'] : NULL;
+$c    = (isset($_GET['c']) && $_GET['c'] != "") ? $_GET['c'] : NULL;
+$m    = (isset($_GET['m']) && $_GET['m'] != "") ? $_GET['m'] : NULL;
+$h    = (isset($_GET['h']) && $_GET['h'] != "") ? $_GET['h'] : NULL;
+$g    = (isset($_GET['g']) && $_GET['g'] != "") ? $_GET['g'] : NULL;
 $title_array = array($env,$c,$h,$g);
 $title = implode(" > ", array_filter($title_array));
 ?>
